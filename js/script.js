e715fedeb31f1b0404fb6ec4f5351db2c84e460e
danyhernandez123 <danydanielhernandezmorales@gmail.com>
2026-06-10 11:29:02 -0500

    Maintenance Page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-const maintenanceMode = true;
-
-if (maintenanceMode) {
-
-  document.body.innerHTML = `
-    <div class="maintenance-screen">
-      <h1>English Star Academy</h1>
-      <h2>Platform Under Maintenance</h2>
-      <p>
-        We are improving the platform.
-        Please come back later.
-      </p>
-    </div>
-  `;
-
-  throw new Error("Maintenance Mode");
-
-}
 
 const lessons = a0Lessons;
 
